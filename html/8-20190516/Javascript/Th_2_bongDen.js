235d82b4396b91bad82b70cde226d80fe3178616
Khanhphan900 <Khanphan900@gmail.com>
2019-05-17 08:50:52 +0700

    add exercise bongDen buoi 8

---
+++ html/8-20190516/Javascript/Th_2_bongDen.js
@@ -0,0 +1,6 @@
+function bongDenOn() {
+  document.getElementById("bongDen").src = "image/pic_bulbon.gif";
+}
+function bongDenOff() {
+  document.getElementById("bongDen").src = "image/pic_bulboff.gif";
+}
