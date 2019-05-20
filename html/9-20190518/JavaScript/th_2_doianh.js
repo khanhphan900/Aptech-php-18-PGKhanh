458da1acecae38944e251df21d81666ae34ca214
Khanhphan900 <Khanphan900@gmail.com>
2019-05-20 21:34:25 +0700

    add Album - 9

--- html/9-20190518/JavaScript/th_2_doianh.js
+++ html/9-20190518/JavaScript/th_2_doianh.js
@@ -1,8 +1,20 @@
-var h_1 = document.getElementById('hinh1');
-var h_2 = document.getElementById('hinh2');
-var h_3 = document.getElementById('hinh3');
+var h_1 = document.getElementById("hinh1");
+var h_2 = document.getElementById("hinh2");
+var h_3 = document.getElementById("hinh3");
 
+console.log(h_1.style.display);
 function slideDoiAnh() {
-    h_1.style.display = 'none';
-    console.log('dong 7');
+  if (h_1.style.display == "block") {
+    h_1.style.display = "none";
+    h_2.style.display = "block";
+    console.log("hien thi dong 9");
+  } else if (h_2.style.display == "block") {
+    h_2.style.display = "none";
+    h_3.style.display = "block";
+  } else {
+    h_3.style.display = "none";
+    h_1.style.display = "block";
+  }
+  setTimeout(slideDoiAnh, 2000);
 }
+setTimeout(slideDoiAnh, 2000);
