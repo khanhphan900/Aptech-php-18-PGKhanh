e508b60a50c94d6bdfc09ece1babf65500dec99f
Khanhphan900 <Khanphan900@gmail.com>
2019-06-08 06:03:31 +0700

    update PHP/table3 08/06/2019

--- PHP/01-0605-PHP/thucHanh/index.php
+++ PHP/01-0605-PHP/thucHanh/index.php
@@ -5,4 +5,28 @@ echo ($ten."  ahihi". "<br>");
 
 echo (5 + "5" . "<br>");
 echo ("5" + 5);
+print "<br>" . $ten ."<br>";
 
+define ("GREETING", "Welcome to W3Schools.com",true);// name of the constant, value of constant,ko phan biet chu hoa chu thuong
+echo GREETINg."<br>"; 
+define("cars", ["Alfa Romeo","BMW","Toyota"],true);
+echo cars[2]."<br>";
+
+define("GREETING", "Welcome to W3Schools.com!");
+
+function myTest() {
+    echo GREETING;
+}
+ 
+myTest();
+
+// if else
+$t = date("H");
+
+if ($t < "10") {
+    echo "Have a good morning!";
+} elseif ($t < "20") {
+    echo "Have a good day!";
+} else {
+    echo "Have a good night!";
+}
