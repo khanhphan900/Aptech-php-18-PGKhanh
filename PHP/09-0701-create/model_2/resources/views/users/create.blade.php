8f130bb4c55e14ee53b0be2855bba66d7f422c4c
khanhphan900 <khanhphan900@gmail.com>
2019-07-01 21:02:40 +0700

    update 01/07

---
+++ PHP/09-0701-create/model_2/resources/views/users/create.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-4 bg-success">
+                <h1> Login </h1>
+                <form action="">
+                    <input class="form-control my-2" name="name" type="text" placeholder="Name...">
+                    <input class="form-control my-2" name="email" type="text" placeholder="Email...">
+                    <input class="form-control my-2" name="password" type="password" placeholder="Password">
+                    
+                </form>
+                
+            </div>
+        </div>
+    </div>
+   
+  </body>
+</html>
---
+++ PHP/09-0701-create/model_2/resources/views/users/show.blade.php
@@ -0,0 +1,4 @@
+<p>{{$user->id}}</p>
+<p>{{$user->name}}</p>
+<p>{{$user->email}}</p>
+{{$user->password}}
